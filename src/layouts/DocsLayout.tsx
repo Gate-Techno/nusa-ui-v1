a448ac97d1606d18f185db7108601c82d4022984
picerld <raficahyadi1221@gmail.com>
2025-03-11 16:47:17 +0700

    [Layout] feat: create layout that included sidebar

---
+++ src/layouts/DocsLayout.tsx
@@ -0,0 +1,59 @@
+import { FC, ReactNode } from "react";
+import { SidebarLeft } from "@/components/container/sidebar-left";
+import { SidebarRight } from "@/components/container/sidebar-right";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbPage,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+
+type SidebarContentItem = {
+  title: string;
+  href: string;
+};
+
+type DocsLayoutProps = {
+  children: ReactNode;
+  sidebarContents?: SidebarContentItem[];
+};
+
+const DocsLayout: FC<DocsLayoutProps> = ({ children, sidebarContents }) => {
+  const defaultSidebarContent: SidebarContentItem[] = [
+    {
+      title: "No Content",
+      href: "#",
+    },
+  ];
+
+  return (
+    <div className="bg-gradient min-h-screen">
+      <SidebarProvider>
+        <SidebarLeft />
+        <SidebarInset>
+          <header className="sticky top-0 flex h-14 items-center gap-2 bg-transparent px-3">
+            <SidebarTrigger />
+            <Separator orientation="vertical" className="h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem>
+                  <BreadcrumbPage className="line-clamp-1">Docs</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </header>
+          <main className="p-4">{children}</main>
+        </SidebarInset>
+        <SidebarRight contents={sidebarContents ?? defaultSidebarContent} />
+      </SidebarProvider>
+    </div>
+  );
+};
+
+export default DocsLayout;
